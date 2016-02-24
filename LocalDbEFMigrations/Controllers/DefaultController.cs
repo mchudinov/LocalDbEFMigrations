ba6b77d8119177b500bf99c8620f61e9557c48da
Mikael Chudinov <mikael@chudinov.net>
2016-02-24 23:43:56 +0100

    Initial commit.

---
+++ LocalDbEFMigrations/Controllers/DefaultController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace LocalDbEFMigrations.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public String Index()
+        {
+            return "Hello World!";
+        }
+    }
+}
---
+++ LocalDbEFMigrations/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LocalDbEFMigrations.MvcApplication" Language="C#" %>
