c142d5b316933adc1dbd4021ae25c18f8ba67ecb
Mikael Chudinov <mikael@chudinov.net>
2016-02-25 00:17:12 +0100

    Add Entity Framework.

---
+++ LocalDbEFMigrations/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using LocalDbEFMigrations.Models;
+
+namespace LocalDbEFMigrations
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext() : base("name=AppDb")
+        {
+            AppDomain.CurrentDomain.SetData("DataDirectory", System.IO.Directory.GetCurrentDirectory());
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+        public DbSet<Widget> Widgets { get; set; }
+    }
+}
---
+++ LocalDbEFMigrations/Models/Widget.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LocalDbEFMigrations.Models
+{
+    public class Widget
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public int Sum { get; set; }
+    }
+}
